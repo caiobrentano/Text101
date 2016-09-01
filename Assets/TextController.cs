62e8c2fba2b0385dc06b28d2152f2ae643dc06f2
Caio Passos <caio.passos@corp.globo.com>
2016-09-01 19:29:36 -0300

    Criacao do metodos de estado

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -5,18 +5,53 @@ using System.Collections;
 public class TextController : MonoBehaviour {
 
 	public Text text;
-	// Use this for initialization
-	void Start () {
-		text.text = "Hello";
+
+	private enum States {cell, cell_mirror, lock_0, lock_1, mirror, sheets_0, sheets_1};
+	private States myState;
 	
+	void Start () {
+		myState = States.cell;
 	}
 	
-	// Update is called once per frame
-	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			text.text = "Space key pressed!";
+	void Update () {
+		if (myState == States.cell) {
+			state_cell ();
+		} else if (myState == States.sheets_0) {
+			state_sheets_0();
+		} else if (myState == States.mirror) {
+			state_mirror();
 		}
-	
+	}
+
+	void state_cell() {
+		text.text = "You are in a prison.\n\n" +
+					"Press S to view Sheets, M to view Mirror and L to view Lock";
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			myState = States.sheets_0;
+		} else if (Input.GetKeyDown (KeyCode.M)) {
+			myState = States.mirror;
+		} else if (Input.GetKeyDown (KeyCode.L)) {
+			// Lock
+		}
+	}
+
+	void state_sheets_0() {
+		text.text = "Look at those dirty sheets!\n\n" +
+			"Press R to Return to roaming your cell.";
+		
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+		}
+	}
+
+	void state_mirror() {
+		text.text = "I'll try to check this Mirror!\n\n" +
+			"Press T to Take the Mirror, R to Return to roaming your cell.";
+		
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+		}
 	}
 }
